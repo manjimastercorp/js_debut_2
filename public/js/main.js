7d672f371dec99b9d549dfb34c154243ca0936c6
manjimaster <master.mlgk.com>
2018-07-03 09:12:31 +0200

    fin creation des personnages et des variables a methodes

---
+++ public/js/main.js
@@ -0,0 +1,69 @@
+var guerrier =
+{
+    nom : "guts",
+    age : 30,
+    taille : 202,
+    attaque : 200,
+    vie : 1000,
+    arme : "epee"
+}
+var mage =
+{
+    nom : "merlin",
+    age : 90,
+    taille : 150,
+    attaque : 150,
+    vie : 500,
+    arme : "lampe magique"
+}
+var paysan =
+{
+    nom : "sebastien",
+    age : 3,
+    taille : 175,
+    attaque : 100,
+    vie : 300,
+    arme : "fourche"
+}
+var noms_concat = [guerrier.nom, mage.nom, paysan.nom];
+// var salutation = ['Bonjour', ' ', 'Alfred', ' ', '!'];
+var ages_concat = [guerrier.age, mage.age, paysan.age];
+var tailles_concat = [guerrier.taille, mage.taille, paysan.taille];
+var attaques_concat = [guerrier.attaque, mage.attaque, paysan.attaque];
+var vies_concat = [guerrier.vie, mage.vie, paysan.vie];
+var armes_concat = [guerrier.arme, mage.arme, paysan.arme];
+
+
+var nom_guerrier_length = guerrier.nom.length;
+var nom_guerrier_charAt = guerrier.nom.charAt(4);
+var nom_guerrier_substr = guerrier.nom.substring(2, 4);
+var age_guerrier_length = guerrier.age.length;
+var taille_guerrier_length = guerrier.taille.length;
+var attaque_guerrier_length = guerrier.attaque.length;
+var vie_guerrier_length = guerrier.vie.length;
+var arme_guerrier_length = guerrier.arme.length;
+var arme_guerrier_charAt = guerrier.arme.charAt(4);
+var arme_guerrier_substr = guerrier.arme.substring(2, 4);
+
+var nom_mage_length = mage.nom.length;
+var nom_mage_charAt = mage.nom.charAt(4);
+var nom_mage_substr = mage.nom.substring(2, 4);
+var age_mage_length = mage.age.length;
+var taille_mage_length = mage.taille.length;
+var attaque_mage_length = mage.attaque.length;
+var vie_mage_length = mage.vie.length;
+var arme_mage_length = mage.arme.length;
+var arme_mage_charAt = mage.arme.charAt(4);
+var arme_mage_substr = mage.arme.substring(2, 4);
+
+var nom_paysan_length = paysan.nom.length;
+var nom_paysan_charAt = paysan.nom.charAt(4);
+var nom_paysan_substr = paysan.nom.substring(2, 4);
+var age_paysan_length = paysan.age.length;
+var taille_paysan_length = paysan.taille.length;
+var attaque_paysan_length = paysan.attaque.length;
+var vie_paysan_length = paysan.vie.length;
+var arme_paysan_length = paysan.arme.length;
+var arme_paysan_charAt = paysan.arme.charAt(4);
+var arme_paysan_substr = paysan.arme.substring(2, 4);
+
